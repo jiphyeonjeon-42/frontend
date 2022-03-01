7cf08bf1df68d0308befba317693d356287af75e
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-03-02 01:25:13 +0900

    REFACTOR : Banner 모바일 반응형 (360px~768px)

--- src/component/main/MainBanner.js
+++ src/component/main/MainBanner.js
@@ -11,11 +11,14 @@ const MainBanner = () => {
         <div className="main-banner__space" />
         <div className="main-banner__wrapper">
           <div className="main-banner__line" />
-          <span className="main-banner__greet font-48-bold color-ff">
+          <div className="main-banner__greet font-48-bold color-ff">
             어서오세요, 집현전입니다.
-          </span>
-          <span className="main-banner__guide font-16 color-d5">
+          </div>
+          <span className="main-banner__guide1 font-16 color-d5">
             집현전에 원하는 책이 있다면, 검색창에 도서를 입력해주세요.
+          </span>
+          <span className="main-banner__guide2 font-16 color-d5">
+            검색창에 원하는 도서를 입력해주세요.
           </span>
           <SearchBar />
         </div>
