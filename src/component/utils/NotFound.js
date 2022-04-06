422cdcb60d5847bf6bf27eecb3b2560045ef6a09
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-04-06 15:15:21 +0900

    FIX: 404페이지 수정

--- src/component/utils/NotFound.js
+++ src/component/utils/NotFound.js
@@ -1,11 +1,9 @@
 import React from "react";
-import BackGround from "./BackGround";
 import "../../css/NotFound.css";
 
 const NotFound = () => {
   return (
     <div>
-      <BackGround page="not-found" />
       <div className="not-found__text color-ff">
         <div className="font-48-bold">404</div>
         <div className="font-40-bold">Not Found</div>
