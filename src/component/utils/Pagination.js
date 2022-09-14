bdcd63e619fc9a45e76de6b9a1ce456ae9c2e5b4
sujikim <sujikim@student.42seoul.kr>
2022-09-14 17:20:04 +0900

    REFACTOR: 페이지네이션 파일 내에 한 컴포넌트만 존재하도록 수정

--- src/component/utils/Pagination.js
+++ src/component/utils/Pagination.js
@@ -3,62 +3,6 @@ import PropTypes from "prop-types";
 import ArrRight from "../../img/arrow_right_black.svg";
 import "../../css/Pagination.css";
 
-const PageButton = ({ pageNum, userPage, setUserPage }) => {
-  const changePage = () => {
-    setUserPage(pageNum);
-  };
-
-  return (
-    <button
-      type="button"
-      onClick={changePage}
-      className={`page-button font-20 ${
-        userPage === pageNum ? "color-54" : "color-a4"
-      }`}
-    >
-      {pageNum}
-    </button>
-  );
-};
-
-const PrePageButton = ({ pageRange, setPageRange }) => {
-  const changePage = () => {
-    if (pageRange > 0) {
-      setPageRange(pageRange - 1);
-    }
-  };
-  return (
-    <button className="pre-page-button" type="button" onClick={changePage}>
-      {pageRange > 0 ? (
-        <img
-          className="page-button-icon-reverse"
-          src={ArrRight}
-          alt="prePage"
-        />
-      ) : (
-        <span className="page-button-icon" />
-      )}
-    </button>
-  );
-};
-
-const NextPageButton = ({ pageRange, setPageRange, lastPage }) => {
-  const changePage = () => {
-    if ((pageRange + 1) * 5 < parseInt(lastPage, 10)) {
-      setPageRange(pageRange + 1);
-    }
-  };
-  return (
-    <button className="next-page-button" type="button" onClick={changePage}>
-      {(pageRange + 1) * 5 < parseInt(lastPage, 10) ? (
-        <img className="page-button-icon" src={ArrRight} alt="nextPage" />
-      ) : (
-        <span className="page-button-icon" />
-      )}
-    </button>
-  );
-};
-
 const Pagination = ({
   userPage,
   setUserPage,
@@ -68,49 +12,65 @@ const Pagination = ({
 }) => {
   const pageRangeArr = [];
 
-  // eslint-disable-next-line no-plusplus
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 5; i += 1) {
     if (pageRange * 5 + i <= lastPage) pageRangeArr.push(pageRange * 5 + i);
   }
+  const changePage = e => {
+    const { value } = e.currentTarget;
+    setUserPage(parseInt(value, 10));
+  };
 
+  const changPageRange = e => {
+    const type = e.currentTarget.value;
+    if (type === "prev" && pageRange > 0) {
+      setPageRange(pageRange - 1);
+    } else if (type === "next" && (pageRange + 1) * 5 < parseInt(lastPage, 10))
+      setPageRange(pageRange + 1);
+  };
   return (
     <div className="admin-pagination">
-      <PrePageButton pageRange={pageRange} setPageRange={setPageRange} />
-      {pageRangeArr.map(n => (
-        <PageButton
-          key={n}
-          pageNum={n}
-          userPage={userPage}
-          setUserPage={setUserPage}
-        />
+      <button
+        className="pre-page-button"
+        type="button"
+        value="prev"
+        onClick={changPageRange}
+      >
+        {pageRange > 0 && (
+          <img
+            className="page-button-icon-reverse"
+            src={ArrRight}
+            alt="prePage"
+          />
+        )}
+      </button>
+      {pageRangeArr.map(pageNum => (
+        <button
+          key={pageNum}
+          type="button"
+          value={pageNum}
+          onClick={changePage}
+          className={`page-button font-20 ${
+            userPage === pageNum ? "color-54" : "color-a4"
+          }`}
+        >
+          {pageNum}
+        </button>
       ))}
-      <NextPageButton
-        pageRange={pageRange}
-        setPageRange={setPageRange}
-        lastPage={lastPage}
-      />
+      <button
+        className="next-page-button"
+        type="button"
+        value="next"
+        onClick={changPageRange}
+      >
+        {(pageRange + 1) * 5 < parseInt(lastPage, 10) && (
+          <img className="page-button-icon" src={ArrRight} alt="nextPage" />
+        )}
+      </button>
     </div>
   );
 };
 
 export default Pagination;
-
-PageButton.propTypes = {
-  pageNum: PropTypes.number.isRequired,
-  userPage: PropTypes.number.isRequired,
-  setUserPage: PropTypes.func.isRequired,
-};
-
-PrePageButton.propTypes = {
-  pageRange: PropTypes.number.isRequired,
-  setPageRange: PropTypes.func.isRequired,
-};
-
-NextPageButton.propTypes = {
-  pageRange: PropTypes.number.isRequired,
-  setPageRange: PropTypes.func.isRequired,
-  lastPage: PropTypes.number.isRequired,
-};
 
 Pagination.propTypes = {
   userPage: PropTypes.number.isRequired,
