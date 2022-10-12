2b5962847591fe06fd1c86d291d55c8e94580105
sujikim <sujikim@student.42seoul.kr>
2022-10-12 21:35:32 +0900

    FIX: useModal toggle대신 open close 함수  별도 제공

--- src/hook/useDialog.js
+++ src/hook/useDialog.js
@@ -1,40 +1,47 @@
 import React, { useState } from "react";
-import useModal from "./useModal";
+import Modal from "../component/utils/Modal";
 import ModalFooter from "../component/utils/ModalFooter";
 import ModalHeader from "../component/utils/ModalHeader";
 import "../css/Dialog.css";
 
 const useDialog = () => {
   // 모달의 기본 로직 + 미리 조립해둔 대화창 형태의 모달 제공
-  const [isOpen, toggleModal, Modal] = useModal();
+  const [isOpen, setIsOpen] = useState(false);
   const [config, setConfig] = useState({
     title: "",
     message: "",
-    afterConfirmFunction: () => {},
+    afterCloseFunction: () => {},
   });
 
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    config.afterCloseFunction();
+    setIsOpen(false);
+  };
+
   const dialogElement = () => {
-    const confirm = () => {
-      toggleModal();
-      config.afterConfirmFunction();
-    };
     return (
       <>
-        <Modal>
-          <ModalHeader
-            title={config.title}
-            isWithCloseButton
-            onCloseModal={toggleModal}
-          />
-          <div className="modal__dialog">
-            <p className="modal__dialog__message">{config.message}</p>
-          </div>
-          <ModalFooter onConfirm={confirm} />
-        </Modal>
+        {isOpen && (
+          <Modal isOpen={isOpen} onCloseModal={closeDialog}>
+            <ModalHeader
+              title={config.title}
+              isWithCloseButton
+              onCloseModal={closeDialog}
+            />
+            <div className="modal__dialog">
+              <p className="modal__dialog__message">{config.message}</p>
+            </div>
+            <ModalFooter onConfirm={closeDialog} />
+          </Modal>
+        )}
       </>
     );
   };
-  return [isOpen, toggleModal, config, setConfig, dialogElement];
+  return [openDialog, closeDialog, config, setConfig, dialogElement];
 };
 
 export default useDialog;
