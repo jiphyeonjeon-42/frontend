c1428a70fe8cbd25185fdae8a22422f7e96d3487
sujikim <sujikim@student.42seoul.kr>
2022-10-13 18:28:08 +0900

    FEAT: EmphasisInString 추가

---
+++ src/data/color.js
@@ -0,0 +1,13 @@
+const colorPalette = [
+  { string: "red", code: "#e63737" },
+  { string: "black", code: "#000000" },
+  { string: "darkgrey1", code: "#2d2d2d" },
+  { string: "darkgrey2", code: "#545454" },
+  { string: "grey1", code: "#818181" },
+  { string: "grey2", code: "#a4a4a4" },
+  { string: "lightgrey1", code: "#d5d5d5" },
+  { string: "lightgrey2", code: "#e8e8e8" },
+  { string: "white", code: "#ffffff" },
+];
+
+export default colorPalette;
