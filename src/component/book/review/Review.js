6edc4778eda6dba444ce5756e7afe60d461c9fdc
김재환 <woghks7859@gmail.com>
2022-11-06 14:54:20 +0900

    FEAT: 리뷰 & 리뷰하기 토글 생성

--- src/component/book/review/Review.js
+++ src/component/book/review/Review.js
@@ -1,31 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import ReviewBox from "./ReviewBox";
+import "../../../css/Tabs.css";
+import "../../../css/Review.css";
 
 // const info = () => ({
 //   bookInfoId: 42,
 //   commentText: "책이 좋네요.",
 // });
+const content = [
+  { name: "리뷰", sort: "showReviews" },
+  { name: "리뷰하기", sort: "doReviews" },
+];
+
+const useFocus = (init, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return null;
+  }
+  const [currentIndex, setCurretIndex] = useState(init);
+  return {
+    currentItem: allTabs[currentIndex].sort,
+    changeItem: setCurretIndex,
+  };
+};
 
 const Review = () => {
-  const [reviewList, setReviewList] = React.useState(null);
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API}/reviews`)
-      .then(response => response.json())
-      .then(json => {
-        setReviewList(json.data.reviewList);
-        console.log(json.data.reviewList);
-      });
-  }, [reviewList]);
-
+  const { currentItem, changeItem } = useFocus(0, content);
   return (
     <div>
-      <div>
-        <h1>토글</h1>
+      <div className="tabs">
+        {content.map((section, index) => (
+          <div
+            className={`tab tab-${
+              section.sort === currentItem ? "on" : "not"
+            }-focus`}
+            role="button"
+            tabIndex={index}
+            onKeyDown=""
+            onClick={() => changeItem(index)}
+          >
+            {section?.name}
+          </div>
+        ))}
       </div>
-      {/* useState 값을 프롭으로 넣으면 될듯 */}
-      {reviewList?.map(review => {
-        return <ReviewBox key={review.bookInfoId} />;
-      })}
+      <div className="tabs-line" />
     </div>
   );
 };
--- src/component/book/review/ReviewBox.js
+++ src/component/book/review/ReviewBox.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 import "../../../css/Review.css";
 
-const ReviewBox = info => {
+const ReviewBox = ({ sort }) => {
+  // sort === "showReview"
   const [fixReview, setFixReview] = useState(false);
-  const [text, setText] = useState(info);
+  const [text, setText] = useState(null);
   const [temp, setTemp] = useState("");
 
+  // sort === "showReview"
   const saveTemp = () => {
-// 리뷰하기부터 진행하고 만들기
+    // 리뷰하기부터 진행하고 만들기
     if (fixReview) {
       setText(temp);
     } else {
@@ -20,54 +24,112 @@ const ReviewBox = info => {
     return setFixReview(!fixReview);
   };
 
-  const fixReviewBtn = e => {
+  const reviewTextArea = e => {
     setText(e.target.value);
   };
 
-  return (
-    <div className="review-box">
-      <div className="review-info">
-        <span className="reviewer-name">리뷰어 이름</span>
-        <span className="review-day">리뷰 날짜</span>
+  // sort === "doReview"
+  const onSubmitHandler = async e => {
+    e.preventDefault();
+    const bookInfoId = 1;
+    const content = e.target.textContent;
+    console.log(content);
+    await axios
+      .post(
+        `${process.env.REACT_APP_API}/reviews`,
+        {
+          bookInfoId: `${bookInfoId}`,
+          content: `${content}`,
+        },
+        {
+          withCredentials: true,
+        },
+      )
+      .then(function print(res) {
+        console.log(res);
+      })
+      .catch(function fail(error) {
+        console.log(error);
+      });
+
+    //     fetch(`${process.env.REACT_APP_API}/reviews`, {
+    //       method: "POST",
+    //       withCredentials: true,
+    //       body: JSON.stringify({
+    //         bookInfoId,
+    //         content,
+    //       }),
+    //     });
+  };
+
+  if (sort === "doReview") {
+    return (
+      <div className="doReview__review-box">
+        <form onSubmit={onSubmitHandler}>
+          <textarea
+            className="review-area"
+            value={text}
+            type="text-area"
+            onChange={reviewTextArea}
+          />
+          {/* 괜찮은 방식인가? */}
+          <br />
+          <input type="submit" />
+        </form>
       </div>
-      <div className="review-content">
-        {fixReview ? (
-          <div>
-            <textarea
-              className="review-content-fix-area"
-              value={text}
-              type="text-area"
-              onChange={fixReviewBtn}
-            />
-          </div>
-        ) : (
-          <div>
-            {/* 텍스트 DB 에서 불러와야 함. */}
-            <span className="reviews-content-area">{text}</span>
-          </div>
-        )}
+    );
+  }
+  if (sort === "showReview") {
+    return (
+      <div className="showReview__review-box">
+        <div className="review-info">
+          <span className="reviewer-name">리뷰어 이름</span>
+          <span className="review-day">리뷰 날짜</span>
+        </div>
+        <div className="review-content">
+          {fixReview ? (
+            <div>
+              <textarea
+                className="review-content-fix-area"
+                value={text}
+                type="text-area"
+                onChange={reviewTextArea}
+              />
+            </div>
+          ) : (
+            <div>
+              {/* 텍스트 DB 에서 불러와야 함. */}
+              <span className="reviews-content-area">{text}</span>
+            </div>
+          )}
+        </div>
+        <div className="review-manage">
+          {fixReview ? (
+            <div>
+              <button type="button" onClick={handleFixBtn}>
+                수정 완료
+              </button>
+              <button type="button" onClick={saveTemp}>
+                취소
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button type="button" onClick={saveTemp}>
+                수정
+              </button>
+              <button type="button">삭제</button>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="review-manage">
-        {fixReview ? (
-          <div>
-            <button type="button" onClick={handleFixBtn}>
-              수정 완료
-            </button>
-            <button type="button" onClick={saveTemp}>
-              취소
-            </button>
-          </div>
-        ) : (
-          <div>
-            <button type="button" onClick={saveTemp}>
-              수정
-            </button>
-            <button type="button">삭제</button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
+  }
+  return null;
 };
 
 export default ReviewBox;
+
+ReviewBox.propTypes = {
+  sort: PropTypes.string.isRequired,
+};
