050c8156899fa716cbf8032bb9e1e34a950b8bd2
sujikim <sujikim@student.42seoul.kr>
2022-11-09 19:31:57 +0900

    FIX: 도서 기부자 상태정보로 이동

--- src/component/book/BookDetail.js
+++ src/component/book/BookDetail.js
@@ -88,18 +88,6 @@ const BookDetail = () => {
                   {bookDetailInfo.category}
                 </div>
               </div>
-              <div className="book-detail__info-wrapper color-54">
-                <div className="book-detail__info-key">기부자</div>
-                <div className="book-detail__info-value">
-                  {bookDetailInfo.books.reduce((accumulator, current) => {
-                    if (!current.donator) return accumulator;
-                    if (accumulator === "" || current.donator === "")
-                      return accumulator + current.donator;
-                    // eslint-disable-next-line prefer-template
-                    return accumulator + ", " + current.donator;
-                  }, "")}
-                </div>
-              </div>
             </div>
             <div className="book-state">
               <span className="book-detail__info-status color-54">
