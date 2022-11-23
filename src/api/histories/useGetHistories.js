d708bd01497587d2948b4b8bb5523449b89cc3dd
yeonseong <yeonseong.dev@gmail.com>
2022-11-23 21:47:27 +0900

    FEAT: useGetHistories

---
+++ src/api/histories/useGetHistories.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { setErrorDialog } from "../../data/error";
+import useApi from "../../hook/useApi";
+import useSearch from "../../hook/useSearch";
+
+const useGetHistories = ({ setOpenTitleAndMessage }) => {
+  const { searchParams, searchResult, setSearchResult, setPage, setQuery } =
+    useSearch();
+  const [who, setWho] = useState("all");
+  const [type, setType] = useState("");
+  const setFilter = (newWho, newType) => {
+    setWho(newWho);
+    setType(newType);
+    setPage(1);
+  };
+
+  const { request, Dialog } = useApi("get", "histories", {
+    query: searchParams.query,
+    page: searchParams.page - 1,
+    limit: 5,
+    type,
+    who,
+  });
+
+  const refineResponse = response => {
+    // TODO: expectedItems 만들기
+    const info = response.data.items;
+    const { totalPages } = response.data.meta;
+    setSearchResult({
+      lastPage: parseInt(totalPages, 10),
+      list: info,
+    });
+  };
+
+  const displayError = error => {
+    setErrorDialog(error, setOpenTitleAndMessage);
+  };
+
+  useEffect(() => {
+    request(refineResponse, displayError);
+  }, [searchParams, who, type]);
+
+  return {
+    historiesList: searchResult.list,
+    lastPage: searchResult.lastPage,
+    page: searchParams.page,
+    setPage,
+    setQuery,
+    setFilter,
+    Dialog,
+  };
+};
+
+export default useGetHistories;
