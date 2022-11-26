aa1cddb8514efd946f3a136051d0b3b000d14691
yeonseong <yeonseong.dev@gmail.com>
2022-11-27 02:34:17 +0900

    FEAT: delete like api hook

---
+++ src/api/like/useDeleteLike.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import getErrorMessage from "../../data/error";
+import useApi from "../../hook/useApi";
+import { compareExpect } from "../../util/typeCheck";
+
+const useDeleteLike = ({ setOpenTitleAndMessage, initBookInfoId }) => {
+  const [bookInfoId, setBookInfoId] = useState(initBookInfoId);
+  const { request } = useApi("delete", `books/info/${bookInfoId}/like`);
+  const [likeData, setLikeData] = useState({});
+
+  const expectedItem = [
+    { key: "bookInfoId", type: "number", isNullable: false },
+    { key: "isLiked", type: "bool", isNullable: false },
+    { key: "likeNum", type: "number", isNullable: false },
+  ];
+
+  const refineResponse = response => {
+    const refinelikeData = compareExpect(
+      `books/info/${initBookInfoId}/like`,
+      [response.data],
+      expectedItem,
+    );
+    setLikeData(...refinelikeData);
+  };
+
+  const displayError = error => {
+    const errorCode = parseInt(error?.response?.data?.errorCode, 10);
+    const [title, message] = getErrorMessage(errorCode).split("\r\n");
+    setOpenTitleAndMessage(title, errorCode ? message : error.message);
+  };
+
+  // bookInfoId가 있을 경우만 api 요청
+  useEffect(() => {
+    if (bookInfoId) request(refineResponse, displayError);
+  }, [bookInfoId]);
+
+  return { setBookInfoId, likeData };
+};
+
+export default useDeleteLike;
