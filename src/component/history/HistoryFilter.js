6e72d235ee13ded41faf673b8ec370cb1ba81ae2
yeonseong <yeonseong.dev@gmail.com>
2022-12-03 14:00:15 +0900

    FEAT: 체크 아이콘 추가

--- src/component/history/HistoryFilter.js
+++ src/component/history/HistoryFilter.js
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import Image from "../utils/Image";
+import CheckIcon from "../../img/check_icon.svg";
+import RedCheckIcon from "../../img/check_icon_red.svg";
 import "../../css/HistoriesFilter.css";
 
 const FilterButton = ({ type, typeName, setType }) => {
@@ -11,9 +14,14 @@ const FilterButton = ({ type, typeName, setType }) => {
       }}
       className="proceeding filter-button"
     >
+      <Image
+        className="filter__icon"
+        src={`${type === typeName ? RedCheckIcon : CheckIcon}`}
+        alt="check"
+      />
       <span
         className={`proceeding-finsh__text ${
-          type === typeName ? "font-16-bold color-54" : "font-16 color-a4"
+          type === typeName ? "font-16-bold color-red" : "font-16 color-a4"
         }`}
       >
         {typeName}
