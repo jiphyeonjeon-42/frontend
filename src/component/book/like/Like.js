b2b8a6b230a5acac81a40852489ca111cae60c68
김재환 <woghks7859@gmail.com>
2022-12-04 11:10:10 +0900

    WIP: 좋아요 파일 분리 (post, delete 후 랜더링이 안되는 문제가 있음)

--- src/component/book/like/Like.js
+++ src/component/book/like/Like.js
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import usePostLike from "../../../api/like/usePostLike";
-import useGetLike from "../../../api/like/useGetLike";
 import useDeleteLike from "../../../api/like/useDeleteLike";
 import "../../../css/BookDetail.css";
 import "../../../css/reset.css";
 import useDialog from "../../../hook/useDialog";
 import ShowLike from "./ShowLike";
-// bookInfoId
-// isLiked
-// likeNum
+
 const Like = ({ initBookInfoId }) => {
-  const [currentLike, setCurrentLike] = useState();
   const { setOpenTitleAndMessage } = useDialog();
-  const { likeData } = useGetLike({
-    setOpenTitleAndMessage,
-    initBookInfoId,
-    setCurrentLike,
-  });
   const { setBookInfoId: setDeleteLike } = useDeleteLike({
     setOpenTitleAndMessage,
   });
@@ -30,26 +21,16 @@ const Like = ({ initBookInfoId }) => {
   const postLike = () => {
     setPostLike(initBookInfoId);
   };
-  const clickLikeHandler = () => {
-    if (currentLike) {
-      deleteLike(initBookInfoId);
-      setCurrentLike(false);
-    } else {
-      postLike(initBookInfoId);
-      setCurrentLike(true);
-    }
-  };
 
   return (
-    <div>
-      <button
-        className="like_button filter-button"
-        type="button"
-        onClick={clickLikeHandler}
-      >
-        <ShowLike likeData={likeData} currentLike={currentLike} />
-      </button>
-    </div>
+    <>
+      <ShowLike
+        setOpenTitleAndMessage={setOpenTitleAndMessage}
+        deleteLike={deleteLike}
+        postLike={postLike}
+        initBookInfoId={initBookInfoId}
+      />
+    </>
   );
 };
 
--- src/component/book/like/ShowLike.js
+++ src/component/book/like/ShowLike.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Image from "../../utils/Image";
 import FilledLike from "../../../img/like_filled.svg";
 import EmptyLike from "../../../img/like_empty.svg";
+import useGetLike from "../../../api/like/useGetLike";
 import "../../../css/BookDetail.css";
 import "../../../css/reset.css";
 
-const ShowLike = ({ likeData, currentLike }) => {
+const ShowLike = ({
+  setOpenTitleAndMessage,
+  deleteLike,
+  postLike,
+  initBookInfoId,
+}) => {
+  const [currentLike, setCurrentLike] = useState();
+  const { likeData } = useGetLike({
+    setOpenTitleAndMessage,
+    initBookInfoId,
+    setCurrentLike,
+  });
+
+  const clickLikeHandler = () => {
+    if (likeData.isLiked) {
+      deleteLike();
+      setCurrentLike(false);
+    } else {
+      postLike();
+      setCurrentLike(true);
+    }
+  };
+
+  useEffect(() => {
+    setCurrentLike(likeData.isLiked);
+  }, []);
+
   console.log(currentLike);
+
   return (
     <div>
-      {currentLike ? (
-        <Image className="like__icon" src={FilledLike} alt="like" />
-      ) : (
-        <Image className="like__icon" src={EmptyLike} alt="unlike" />
-      )}
-      {`좋아요 ${likeData.likeNum}`}
+      <button
+        className="like_button filter-button"
+        type="button"
+        onClick={clickLikeHandler}
+      >
+        {likeData.isLiked ? (
+          <Image className="like__icon" src={FilledLike} alt="like" />
+        ) : (
+          <Image className="like__icon" src={EmptyLike} alt="unlike" />
+        )}
+        {`좋아요 ${likeData.likeNum}`}
+      </button>
     </div>
   );
 };
@@ -23,17 +57,21 @@ const ShowLike = ({ likeData, currentLike }) => {
 export default ShowLike;
 
 ShowLike.propTypes = {
-  currentLike: PropTypes.number.isRequired,
-  likeData: PropTypes.shape({
-    bookInfoId: PropTypes.number.isRequired,
-    isLiked: PropTypes.bool,
-    likeNum: PropTypes.number,
-  }),
+  setOpenTitleAndMessage: PropTypes.func.isRequired,
+  deleteLike: PropTypes.func.isRequired,
+  postLike: PropTypes.func.isRequired,
+  initBookInfoId: PropTypes.number.isRequired,
+  // currentLike: PropTypes.number.isRequired,
+  // likeData: PropTypes.shape({
+  //   bookInfoId: PropTypes.number.isRequired,
+  //   isLiked: PropTypes.bool,
+  //   likeNum: PropTypes.number,
+  // }),
 };
 
-ShowLike.defaultProps = {
-  likeData: {
-    isLiked: false,
-    likeNum: 0,
-  },
-};
+// ShowLike.defaultProps = {
+//   likeData: {
+//     isLiked: false,
+//     likeNum: 0,
+//   },
+// };
