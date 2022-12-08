4b6a8408375242ea8ad6a38188a27129e4147410
김재환 <woghks7859@gmail.com>
2022-12-08 20:35:40 +0900

    FIX: 글자수 420자 수정

--- src/component/book/review/usePostReview.js
+++ src/component/book/review/usePostReview.js
@@ -14,7 +14,7 @@ const usePostReview = ({ setOpenTitleAndMessage, bookInfoId, changeTab }) => {
   };
 
   const displayError = async () => {
-    setOpenTitleAndMessage("10자 이상 100자 이하로 입력해주세요.", "");
+    setOpenTitleAndMessage("10자 이상 420자 이하로 입력해주세요.", "");
   };
 
   useEffect(() => {
