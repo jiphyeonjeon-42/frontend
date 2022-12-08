f1ccc1509dcf8ae6b2b4e2189915915285c5c9e8
김재환 <woghks7859@gmail.com>
2022-12-08 21:29:31 +0900

    FIX: 마이페이지_공개 리뷰만 보여주기

--- src/component/mypage/MyReview.js
+++ src/component/mypage/MyReview.js
@@ -4,11 +4,11 @@ import Reserve from "../../img/list-check-solid.svg";
 import HandleReview from "../book/review/HandleReview";
 import axiosPromise from "../../util/axios";
 import Pagination from "../utils/Pagination";
-import useGetReviewInfo from "../../api/review/useGetReviewInfo";
+import useGetMyReviewInfo from "../../api/review/useGetMyReviewInfo";
 
 const MyReview = () => {
   const { page, setPage, lastPage, reviewList, setReviewList } =
-    useGetReviewInfo();
+    useGetMyReviewInfo();
 
   const deleteReview = reviewId => {
     const temp = reviewList.filter(review => review.reviewsId !== reviewId);
@@ -27,15 +27,17 @@ const MyReview = () => {
           ENsize="font-14"
         />
         <div className="mypage-inquire-box-long">
-          {reviewList.map(data => (
-            <HandleReview
-              key={data.reviewsId}
-              data={data}
-              nickname={data.nickname}
-              createdAt={data.createdAt}
-              onClickDel={deleteReview}
-            />
-          ))}
+          {reviewList.map(data =>
+            data.disabled === 0 ? (
+              <HandleReview
+                key={data.reviewsId}
+                data={data}
+                nickname={data.nickname}
+                createdAt={data.createdAt}
+                onClickDel={deleteReview}
+              />
+            ) : null,
+          )}
           <div className="return-book-table__pagination">
             <Pagination page={page} setPage={setPage} lastPage={lastPage} />
           </div>
