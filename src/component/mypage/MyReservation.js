07df7b69e9decb4f4bc425e2f488ebb7a8abd22a
sujikim <sujikim@student.42seoul.kr>
2022-12-10 20:31:50 +0900

    CHORE: 불필요한 주석 삭제

--- src/component/mypage/MyReservation.js
+++ src/component/mypage/MyReservation.js
@@ -1,19 +1,11 @@
-// import React, { useEffect, useState } from "react";
 import React from "react";
-// import { Link, useSearchParams } from "react-router-dom";
 import useDialog from "../../hook/useDialog";
 import useGetUsersSearchId from "../../api/users/useGetUsersSearchId";
 import RentedOrReservedBooks from "./MyRentInfo/RentedOrReservedBooks";
-// import ScrollTopButton from "../utils/ScrollTopButton";
 import InquireBoxTitle from "../utils/InquireBoxTitle";
-// import getErrorMessage from "../../data/error";
-// import Login from "../../img/login_icon_white.svg";
-// import Book from "../../img/admin_icon.svg";
 import Reserve from "../../img/list-check-solid.svg";
-// import "../../css/Mypage.css";
 
 const MyReservation = () => {
-  // const [urlQuery, setUrlQuery] = useSearchParams();
   const {
     setOpen: openDialog,
     config: dialogConfig,
@@ -27,70 +19,6 @@ const MyReservation = () => {
     setDialogTitleAndMessage,
     userId,
   });
-  // const [deviceMode, setDeviceMode] = useState(window.innerWidth);
-
-  // const convertRoleToStr = roleInt => {
-  //   switch (roleInt) {
-  //     case 1:
-  //       return "카뎃";
-  //     case 2:
-  //       return "사서";
-  //     case 3:
-  //       return "운영진";
-  //     default:
-  //       return "미인증";
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const error = urlQuery.get("errorCode");
-  //   if (error) {
-  //     const errorCode = parseInt(error, 10);
-  //     const [title, message] = getErrorMessage(errorCode).split("\r\n");
-  //     setDialogTitleAndMessage(title, message, () => {
-  //       urlQuery.delete("errorCode");
-  //       setUrlQuery(urlQuery);
-  //     });
-  //   }
-
-  //   const getWindowWidth = () => {
-  //     if (window.innerWidth >= 1200) setDeviceMode("desktop");
-  //     if (window.innerWidth < 1200 && window.innerWidth > 767)
-  //       setDeviceMode("pad");
-  //     if (window.innerWidth < 767) setDeviceMode("mobile");
-  //   };
-  //   window.addEventListener("resize", getWindowWidth);
-  //   return () => {
-  //     window.removeEventListener("resize", getWindowWidth);
-  //   };
-  // }, []);
-
-  // const concatDate = day => {
-  //   let overDueDate = "";
-
-  //   day.setDate(day.getDate() + userInfo.overDueDay);
-  //   overDueDate += day.getFullYear();
-  //   overDueDate += "-";
-  //   overDueDate +=
-  //     day.getMonth() + 1 >= 10
-  //       ? day.getMonth() + 1
-  //       : "0".concat(day.getMonth() + 1);
-  //   overDueDate += "-";
-  //   overDueDate +=
-  //     day.getDate() >= 10 ? day.getDate() : "0".concat(day.getDate());
-  //   return overDueDate;
-  // };
-
-  // const getOverDueDate = () => {
-  //   if (
-  //     !userInfo.penaltyEndDate ||
-  //     new Date(userInfo.penaltyEndDate).setHours(0, 0, 0, 0) <
-  //       new Date().setHours(0, 0, 0, 0)
-  //   ) {
-  //     return concatDate(new Date());
-  //   }
-  //   return concatDate(new Date(userInfo.penaltyEndDate));
-  // };
 
   return (
     <>
