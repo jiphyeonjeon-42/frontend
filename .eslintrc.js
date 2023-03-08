db5da2183b4cc88e093fa540328db7cba7ebd545
scarf <greenscarf005@gmail.com>
2023-03-08 09:18:53 +0900

    chore: eslint 설정 정리
    - `package.json`의 `eslintConfig`를 `.eslintrc.js`로 이동
    - 타입스크립트 룰 추가
    - babel 없이도 optional chaining(?.) 연산자 사용 가능하도록 ecmaVersion 2020으로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,61 @@
-// .eslintrc.js
+// @ts-check
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { defineConfig } = require("eslint-define-config");
 
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
+module.exports = defineConfig({
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: { jsx: true },
   },
+  env: { browser: true, es6: true, node: true },
   extends: [
+    "react-app",
+    "react-app/jest",
     "airbnb",
     "prettier",
     "eslint:recommended",
+    /**
+     * "extends": ["plugin:prettier/recommended"]는 세 가지를 한다고 설명한다.
+     * 1. eslint-plugin-prettier를 실행한다.(Enables eslint-plugin-prettier.)
+     * 2. prettier/prettier의 규칙을 "error"로 설정한다.(Sets the prettier/prettier rule to "error".)
+     * 3. eslint-config-prettier를 적용시킨다.(Extends the eslint-config-prettier configuration.)
+     */
     "plugin:prettier/recommended",
   ],
   rules: {
-    "react/jsx-filename-extension": [
-      "error",
-      {
-        extensions: [".js", ".jsx"],
-      },
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
     "no-console": 0,
+    "react/react-in-jsx-scope": "off" /**  import React 생략 */,
   },
-  settings: {
-    "import/resolver": {
-      node: {
-        paths: ["src"],
+  overrides: [
+    {
+      files: ["**/*.jsx"],
+      rules: { "react/forbid-prop-types": "off" } /** PropTypes.object 사용 */,
+    },
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      rules: {
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error"],
       },
     },
+    {
+      files: ["vite.config.ts"],
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
+  settings: {
+    "import/resolver": { node: { paths: ["src"] } },
   },
-  parser: "babel-eslint",
-};
-
-//   "extends": ["plugin:prettier/recommended"]는 세 가지를 한다고 설명한다.
-// 1. eslint-plugin-prettier를 실행한다.(Enables eslint-plugin-prettier.)
-// 2. prettier/prettier의 규칙을 "error"로 설정한다.(Sets the prettier/prettier rule to "error".)
-// 3. eslint-config-prettier를 적용시킨다.(Extends the eslint-config-prettier configuration.)
+});
