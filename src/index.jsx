f4f9fedfe1a4ebb62d92a35dbcb487c6464f02e0
sujikim <sujikim@student.42seoul.kr>
2023-04-13 15:50:57 +0900

    refactor: sentry dsn값 env로 변경 (#410)

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./index.css";
 
 Sentry.init({
-  dsn: "https://a03d63cfc79147d292b9b09630e70e4a@o4504156227567616.ingest.sentry.io/4504156228485120",
+  dsn: import.meta.env.REACT_APP_SENTRY,
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
   environment:
