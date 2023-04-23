e20299a4481d2f74131231691f9e41bc464932cd
scarf <greenscarf005@gmail.com>
2023-04-23 09:42:32 +0900

    refactor: useSearch에서 proptype 제거
    useSearch는 어떤 인자도 받지 않기 때문에 사용하지 않는 proptypes import를 제거했습니다

--- src/hook/useSearch.js
+++ src/hook/useSearch.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
 import useDebounce from "./useDebounce";
 
 const useSearch = () => {
@@ -51,7 +50,3 @@ const useSearch = () => {
 };
 
 export default useSearch;
-
-useSearch.propTypes = {
-  searchFuction: PropTypes.func.isRequired,
-};
