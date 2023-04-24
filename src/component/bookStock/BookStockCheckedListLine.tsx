40f1edc11eedbbdbb30cb28c79034d2a976f273d
scarf <greenscarf005@gmail.com>
2023-04-24 20:41:30 +0900

    refactor: `BookStockCheckedListLine` tsx로 변경

---
+++ src/component/bookStock/BookStockCheckedListLine.tsx
@@ -0,0 +1,22 @@
+import { Book } from "../../types";
+import InquireBoxItem from "../utils/InquireBoxItem";
+import InquireBoxLine from "../utils/InquireBoxLine";
+
+type Props = {
+  book: Book;
+};
+
+const BookStockCheckedListLine = ({
+  book: { bookId, callSign, category, title },
+}: Props) => {
+  return (
+    <InquireBoxLine>
+      <InquireBoxItem keyString="bookId" value={bookId} />
+      <InquireBoxItem keyString="callSign" value={callSign} />
+      <InquireBoxItem keyString="category" value={category} />
+      <InquireBoxItem keyString="title" value={title} />
+    </InquireBoxLine>
+  );
+};
+
+export default BookStockCheckedListLine;
