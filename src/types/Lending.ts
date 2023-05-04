346d4f8e320022f36cfc693a8ae34329403dab0d
scarf <greenscarf005@gmail.com>
2023-05-04 13:36:22 +0900

    refactor: type 디렉터리로 정리

---
+++ src/types/Lending.ts
@@ -0,0 +1,11 @@
+export type Lending = {
+  id: number;
+  title: string;
+  callSign: string;
+  dueDate: string;
+  createdAt: string;
+  lendingCondition: string;
+  login?: string;
+  penaltyDays: number;
+  image?: string;
+};
