caff5336e0096d1554a347e6107b33c29384e3f9
sujikim <sujikim@student.42seoul.kr>
2023-05-16 17:17:02 +0900

    feat: Date 컴포넌트 추가
    기본적으로 연도도 노출하지만 반응형으로 모바일화면에서는 월-일만 노출하는 컴포넌트

---
+++ src/component/utils/Date.tsx
@@ -0,0 +1,16 @@
+import "../../css/Date.css";
+
+type DateProps = {
+  className?: string;
+  date: string;
+};
+const Date = ({ date, className }: DateProps) => {
+  return (
+    <span className={`date__wrapper ${className}`}>
+      <span className="date__year">{date.slice(0, 5)}</span>
+      <span className="date__date">{date.slice(5, 10)}</span>
+    </span>
+  );
+};
+
+export default Date;
