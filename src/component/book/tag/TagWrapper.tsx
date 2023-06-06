3dff9a1a4c8ce81ce5abc110c0ffadd35e0cb420
chanhihi <chanhihi55@gmail.com>
2023-06-06 21:11:22 +0900

    feat(tagWrapper): implement tagWrapper

---
+++ src/component/book/tag/TagWrapper.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import TagList from "./TagList";
+import Button from "../../utils/Button";
+import useApi from "../../../hook/useApi";
+import { AxiosResponse } from "axios";
+
+type TagProps = {
+  bookInfoId: string;
+};
+
+type TagData = {
+  id: number;
+  content: string;
+  count: number;
+  login: string;
+};
+
+const TagWrapper = ({ bookInfoId }: TagProps) => {
+  const [tagData, setTagData] = useState<TagData[]>([]);
+  const { request, Dialog } = useApi("get", `/tags/${bookInfoId}`);
+
+  console.log("tag wrapper");
+
+  useEffect(() => {
+    const getTagRequest = (res: AxiosResponse) => {
+      setTagData(res.data);
+      console.log("태그 데이터 >> ", res.data);
+    };
+    request(getTagRequest);
+  }, []);
+
+  return (
+    <div className="none-drag">
+      <Dialog />
+      <TagList tagData={tagData} />
+    </div>
+  );
+};
+
+export default TagWrapper;
