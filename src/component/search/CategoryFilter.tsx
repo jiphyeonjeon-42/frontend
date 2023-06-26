99a8095aa1daff45c2eae7eb27e7cd511fbcade2
sujikim <sujikim@student.42seoul.kr>
2023-06-26 20:57:32 +0900

    refactor: search/CategoryFilter 가독성 개선
    1. 변수명 변경 "userCate" 같은 모호한 표현 대신 selectedCategory 등으로 명시
    2. DOM 접근방식 개선 querySelector -> useRef
    3. 복잡한 for문의 합계 대신 array filter 및 getBoundingClientRect를 활용

--- src/component/search/CategoryFilter.tsx
+++ src/component/search/CategoryFilter.tsx
@@ -1,226 +1,110 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import Image from "../utils/Image";
+import Category from "./Category";
 import ArrLeftGray from "../../asset/img/arrow_left_gray.svg";
 import ArrLeftBlack from "../../asset/img/arrow_left_black.svg";
 import ArrRightGray from "../../asset/img/arrow_right_gray.svg";
 import ArrRightBlack from "../../asset/img/arrow_right_black.svg";
 import "../../asset/css/CategoryFilter.css";
 
-const MARGIN_OF_CATEGORY_BUTTON = 36;
-const EPSILON = 1;
-
-type PreCategoryProps = {
-  startOfScroll: boolean;
+type Props = {
+  selectedCategory: number;
+  categoryList: { name: string; count: number }[];
 };
 
-const PreCategory = ({ startOfScroll }: PreCategoryProps) => {
-  const scrollToPre = () => {
-    const categories = document.querySelector(".categories");
-    if (categories) {
-      const categoriesScrollX = categories.scrollLeft;
+const CategoryFilter = ({ selectedCategory, categoryList }: Props) => {
+  const categoriesRef = useRef<HTMLDivElement>(null);
+  // categoriesRef의 스크롤이 처음이거나 끝인지 판단하는 state
+  const [scrollPosition, setScrollPosition] = useState({
+    isScrollAtStart: true,
+    isScrollAtEnd: true,
+  });
 
-      const categoryButton = document.getElementsByClassName("category-button");
-      const categoryButtonWidth = Array.from(categoryButton).map(
-        items => items.clientWidth + MARGIN_OF_CATEGORY_BUTTON,
-      );
-
-      let sumOfCategory = 0;
-      // eslint-disable-next-line no-plusplus
-      for (let index = 0; index < categoryButtonWidth.length; index++) {
-        if (
-          sumOfCategory + categoryButtonWidth[index] + EPSILON >=
-          categoriesScrollX
-        ) {
-          break;
-        }
-        sumOfCategory += categoryButtonWidth[index];
-      }
-      categories.scrollTo({ left: sumOfCategory, top: 0, behavior: "smooth" });
+  const updateScrollPosition = () => {
+    const target = categoriesRef.current;
+    if (target) {
+      const endOfScroll = target.scrollWidth - target.offsetWidth;
+      setScrollPosition({
+        isScrollAtStart: target.scrollLeft === 0,
+        isScrollAtEnd: target.scrollLeft === endOfScroll,
+      });
     }
   };
 
-  return (
-    <button className="pre-category" type="button" onClick={scrollToPre}>
-      {startOfScroll ? (
-        <Image
-          className="category-button-icon"
-          src={ArrLeftGray}
-          alt="preCategory"
-        />
-      ) : (
-        <Image
-          className="category-button-icon"
-          src={ArrLeftBlack}
-          alt="preCategory"
-        />
-      )}
-    </button>
-  );
-};
+  useEffect(() => {
+    updateScrollPosition();
+    // 윈도우 창 크기가 변할 때마다 updateScrollPosition 함수를 실행
+    window.addEventListener("resize", updateScrollPosition);
+    return () => {
+      window.removeEventListener("resize", updateScrollPosition);
+    };
+  }, [categoriesRef]);
 
-type NextCategoryProps = {
-  endOfScroll: boolean;
-};
+  const scrollTo = (direction: "prev" | "next") => {
+    if (categoriesRef.current) {
+      const childElements = Array.from(categoriesRef.current.children);
 
-const NextCategory = ({ endOfScroll }: NextCategoryProps) => {
-  const scrollToNext = () => {
-    const categories = document.querySelector<HTMLDivElement>(".categories");
-    if (categories) {
-      const categoriesScrollX = categories.scrollLeft;
-      const categoriesOffsetWidth = categories.offsetWidth;
-      const categoriesScrollWidth = categories.scrollWidth;
-      const endOfScrollWidth = categoriesScrollWidth - categoriesOffsetWidth;
+      // categories의 왼쪽 경계값 기준
+      const edge = categoriesRef.current.getBoundingClientRect().left;
+      const target =
+        direction === "prev"
+          ? childElements
+              // 경계보다 작은 left값을 가진 마지막 요소
+              .filter(e => Math.ceil(e.getBoundingClientRect().left) < edge)
+              .slice(-1)[0]
+          : childElements
+              // 경계보다 큰 left값을 가진 첫번째 요소
+              .filter(e => Math.floor(e.getBoundingClientRect().left) > edge)
+              .slice()[0];
 
-      const categoryButton = document.getElementsByClassName("category-button");
-      const categoryButtonWidth = Array.from(categoryButton).map(
-        items => items.clientWidth + MARGIN_OF_CATEGORY_BUTTON,
-      );
-
-      let sumOfCategory = 0;
-      // eslint-disable-next-line no-plusplus
-      for (let index = 0; index < categoryButtonWidth.length; index++) {
-        sumOfCategory += categoryButtonWidth[index];
-        if (sumOfCategory - EPSILON > categoriesScrollX) {
-          break;
-        }
-      }
-      categories.scrollTo({
-        left:
-          sumOfCategory > endOfScrollWidth ? endOfScrollWidth : sumOfCategory,
-        top: 0,
+      // 해당 요소로 가로 스크롤 부드럽게 이동
+      target?.scrollIntoView({
+        inline: "start",
+        block: "nearest",
         behavior: "smooth",
       });
     }
   };
 
   return (
-    <button className="next-category" type="button" onClick={scrollToNext}>
-      {endOfScroll ? (
-        <Image
-          className="category-button-icon"
-          src={ArrRightGray}
-          alt="preCategory"
-        />
-      ) : (
-        <Image
-          className="category-button-icon"
-          src={ArrRightBlack}
-          alt="nextCategory"
-        />
-      )}
-    </button>
-  );
-};
-
-type CategoryProps = {
-  userWord: string;
-  userSort: string;
-  userCate: number;
-  categoryIndex: number;
-  categoryName: string;
-  categoryNum: number;
-};
-
-const Category = ({
-  userWord,
-  userSort,
-  userCate,
-  categoryIndex,
-  categoryName,
-  categoryNum,
-}: CategoryProps) => {
-  const navigate = useNavigate();
-
-  const changeFilter = () => {
-    navigate(
-      `?search=${userWord}&page=${1}&category=${categoryName}&sort=${userSort}`,
-    );
-  };
-
-  return (
-    <button
-      className={`category-button button-onclick-${userCate === categoryIndex}`}
-      type="button"
-      onClick={changeFilter}
-    >
-      <div
-        className={`${
-          userCate === categoryIndex
-            ? "font-16-bold color-54"
-            : "font-16 color-a4"
-        } category-button-text cateNum-${categoryIndex}`}
-      >
-        {categoryName} ({categoryNum})
-      </div>
-    </button>
-  );
-};
-
-type CategoryFilterProps = {
-  userWord: string;
-  userSort: string;
-  userCate: number;
-  entireCate: { name: string; count: number }[];
-};
-
-const CategoryFilter = ({
-  userWord,
-  userSort,
-  userCate,
-  entireCate,
-}: CategoryFilterProps) => {
-  const [startOfScroll, setStartOfScroll] = useState(true);
-  const [endOfScroll, setEndOfScroll] = useState(true);
-
-  const setScrollState = () => {
-    const categories = document.querySelector<HTMLDivElement>(".categories");
-    if (categories) {
-      const categoriesScrollX = categories.scrollLeft;
-      const categoriesOffsetWidth = categories.offsetWidth;
-      const categoriesScrollWidth = categories.scrollWidth;
-      const endOfScrollWidth = categoriesScrollWidth - categoriesOffsetWidth;
-
-      if (categoriesScrollX === 0) {
-        setStartOfScroll(true);
-      } else {
-        setStartOfScroll(false);
-      }
-
-      if (categoriesScrollX === endOfScrollWidth) {
-        setEndOfScroll(true);
-      } else {
-        setEndOfScroll(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    setScrollState();
-    window.addEventListener("resize", setScrollState);
-    return () => {
-      window.removeEventListener("resize", setScrollState);
-    };
-  }, [setScrollState]);
-
-  return (
     <div className="category-filter">
       <div className="category-filter__list">
-        <PreCategory startOfScroll={startOfScroll} />
-        <div className="categories" onScroll={setScrollState}>
-          {entireCate.map((items, index) => (
+        <button
+          className="pre-category"
+          type="button"
+          onClick={() => scrollTo("prev")}
+        >
+          <Image
+            className="category-button-icon"
+            src={scrollPosition.isScrollAtStart ? ArrLeftGray : ArrLeftBlack}
+            alt="preCategory"
+          />
+        </button>
+        <div
+          className="categories"
+          onScroll={updateScrollPosition}
+          ref={categoriesRef}
+        >
+          {categoryList.map((items, index) => (
             <Category
               key={items.name}
-              userWord={userWord}
-              userSort={userSort}
-              userCate={userCate}
-              categoryIndex={index}
-              categoryName={items.name}
-              categoryNum={items.count}
+              name={items.name}
+              count={items.count}
+              isSelected={selectedCategory === index}
             />
           ))}
         </div>
-        <NextCategory endOfScroll={endOfScroll} />
+        <button
+          className="next-category"
+          type="button"
+          onMouseUp={() => scrollTo("next")}
+        >
+          <Image
+            className="category-button-icon"
+            src={scrollPosition.isScrollAtEnd ? ArrRightGray : ArrRightBlack}
+            alt="nextCategory"
+          />
+        </button>
       </div>
       <div className="category-filter__line" />
     </div>
