32230e104078d51448a0e8d3dc64024641e9deac
scarf <greenscarf005@gmail.com>
2023-07-07 12:04:23 +0900

    feat: 리뷰 공개/비공개 전환에 react query 사용

--- src/api/reviews/usePatchReviewsId.ts
+++ src/api/reviews/usePatchReviewsId.ts
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
 import { useApi } from "../../hook/useApi";
 import { useNewDialog } from "../../hook/useNewDialog";
+import { client } from "../../util/tsRestClient";
 
 export const usePatchReviewsId = () => {
-  const [reviewId, setReviewId] = useState<number>();
-  const { request } = useApi("patch", `reviews/${reviewId}`);
+  const { addErrorDialog, addDialogWithTitleAndMessage } = useNewDialog();
 
-  const { addDialogWithTitleAndMessage } = useNewDialog();
-  const onSuccess = () => {
-    addDialogWithTitleAndMessage("patched", "처리되었습니다", "", () =>
-      window.location.reload(),
-    );
-  };
-
-  useEffect(() => {
-    if (reviewId !== undefined) {
-      request(onSuccess);
-    }
-  }, [reviewId]);
-  return { setReviewId };
+  const mutation = client.reviews.patch.useMutation({
+    onSuccess: () => {
+      console.log("local success");
+      addDialogWithTitleAndMessage("patched", "처리되었습니다", "", () =>
+        window.location.reload(),
+      );
+    },
+    onError: err => {
+      switch (err.status) {
+        case 401:
+          return addErrorDialog({ response: { data: err.body } });
+        case 404:
+          return addDialogWithTitleAndMessage(
+            err.body.toString(),
+            "리뷰 검색 실패",
+            "검색한 리뷰가 존재하지 않습니다",
+          );
+        default:
+          return addErrorDialog(err);
+      }
+    },
+  });
+  return mutation;
 };
