adbf24ba81b4117deabce7ad2c414fd1f3ed89ec
sujikim <sujikim@student.42seoul.kr>
2023-07-18 07:16:27 +0900

    fix: POST reviews 성공시 성공알림 추가

--- src/api/reviews/usePostReview.ts
+++ src/api/reviews/usePostReview.ts
@@ -11,14 +11,20 @@ type Props = {
 export const usePostReview = ({ bookInfoId, resetTab }: Props) => {
   const [content, setContent] = useState("");
 
-  const { addErrorDialog } = useNewDialog();
+  const { addDialogWithTitleAndMessage, addErrorDialog } = useNewDialog();
+
+  const displaySuccessAndResetTab = () => {
+    const title = "성공적으로 등록되었습니다";
+    addDialogWithTitleAndMessage(title, title, "", resetTab);
+  };
 
   const request = () =>
     axiosPromise("post", "/reviews", {
       bookInfoId,
       content,
     })
-      .then(resetTab)
+      .then(displaySuccessAndResetTab)
       .catch(addErrorDialog);
+
   return { content, setContent, request };
 };
