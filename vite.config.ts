8a722438b364bb341d9deaa342d8197e57d266c1
scarf <greenscarf005@gmail.com>
2023-08-18 11:29:57 +0900

    build: 절대경로 import 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig, loadEnv } from "vite";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 /**
  * REACT_APP_ 으로 시작하는 환경변수를
@@ -21,7 +22,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     /** @see https://vitejs.dev/plugins/ */
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
 
     envPrefix,
 
