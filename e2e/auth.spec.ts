fd057e795eb6f5bc62320fd6cea3cc02c84fe44b
scarf <greenscarf005@gmail.com>
2023-08-23 19:25:09 +0900

    test: 인증 및 서버에 부하를 주는 행동 생략

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('42 로그인 페이지 이동', async ({ page }) => {
+test.skip('42 로그인 페이지 이동', async ({ page }) => {
   await page.goto('http://localhost:4242/');
   await page.getByRole('link', { name: 'login 로그인' }).click();
   await page.getByRole('link', { name: '42 intra 로그인' }).click();
