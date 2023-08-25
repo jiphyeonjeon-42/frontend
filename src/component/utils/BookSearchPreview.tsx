8bb832750bf62ae39e9bc8a7c6cd13849091f09e
sujikim <sujikim@student.42seoul.kr>
2023-08-25 17:13:50 +0900

    fix: 검색결과 미리보기 Pagination 방식 변경

--- src/component/utils/BookSearchPreview.tsx
+++ src/component/utils/BookSearchPreview.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useGetSearchKeywordsAutocomplete } from "~/api/searchKeywords/useGetSearchKeywordsAutocomplete";
 import { type BookInfo } from "~/type";
 import BookSearchPreviewList from "~/component/utils/BookSearchPreviewList";
-import PaginationCircle from "~/component/utils/PaginationCircle";
+import Paginations from "~/component/utils/Paginations";
 import "~/asset/css/BookSearchPreview.css";
 
 export type BookPreviewType = Omit<BookInfo, "category">;
@@ -16,20 +16,25 @@ const PAGE_SIZE = 3;
 
 const BookSearchPreview = ({ keyword }: Props) => {
   const { books } = useGetSearchKeywordsAutocomplete();
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
 
-  const slicedBooks = books.slice(page * PAGE_SIZE, (page + 1) * PAGE_SIZE);
+  const slicedBooks = books.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
   const pageCount = Math.floor(books.length / PAGE_SIZE);
 
   return (
     <div className="search-preview__wrapper">
       <BookSearchPreviewList keyword={keyword} books={slicedBooks} />
-      <PaginationCircle
+      <Paginations.Root
         className="search-preview__pagination"
-        currentPage={page}
-        setCurrentPage={setPage}
-        length={pageCount}
-      />
+        page={page}
+        setPage={setPage}
+        lastPage={pageCount}
+      >
+        <Paginations.Prev />
+        <span className="search-preview__page">{page}</span>
+        <span className="search-preview__last-page">{pageCount}</span>
+        <Paginations.Next />
+      </Paginations.Root>
       <Link
         className="search-preview__more"
         to={`/search?search=${encodeURI(keyword)}`}
