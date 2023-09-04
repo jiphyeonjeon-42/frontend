d23abd7b94d8ff4772df807d5a2cf415f0f0e49f
sujikim <sujikim@student.42seoul.kr>
2023-09-04 19:01:39 +0900

    fix: 메인 추천도서 UI 피드백 반영, 로딩 추가 및 리스트 하나일때 자동 애니메이션 해제

--- src/component/main/MainRecommend.tsx
+++ src/component/main/MainRecommend.tsx
@@ -2,17 +2,26 @@ import MainRecommendTitle from "~/component/main/MainRecommendTitle";
 import MainRecommendList from "~/component/main/MainRecommendList";
 import "~/asset/css/MainRecommend.css";
 import { useGetCursusRecommendBooks } from "~/api/cursus/useGetCursusRecommendBooks";
+import Loader from "../utils/Loader";
 
 const MainRecommend = () => {
-  const { books, options, setSelectedOption } = useGetCursusRecommendBooks();
+  const { books, options, setSelectedOption, isLoading } =
+    useGetCursusRecommendBooks();
   return (
     <section className="main__recommend__wrapper">
       <MainRecommendTitle
+        isLoading={isLoading}
         options={options}
         setSelectedOption={setSelectedOption}
         description="과제를 진행하는 데 도움이 되는 추천 도서 목록입니다."
       />
-      <MainRecommendList books={books} />
+      {isLoading ? (
+        <div className="main__recommend__loader">
+          <Loader />
+        </div>
+      ) : (
+        <MainRecommendList books={books} isLoading={isLoading} />
+      )}
     </section>
   );
 };
