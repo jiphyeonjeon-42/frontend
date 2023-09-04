db8d1c207aac92debbc7f98ab147d17cf6abc61c
scarf <greenscarf005@gmail.com>
2023-09-04 17:02:16 +0900

    refactor: useGetReviewInfo에 userAtom 적용

--- src/api/reviews/useGetReviewInfo.ts
+++ src/api/reviews/useGetReviewInfo.ts
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
-import { useApi } from "../../hook/useApi";
+import { useApi } from "~/hook/useApi";
+import { userIdAtom } from "~/atom/userAtom";
+import { useRecoilValue } from "recoil";
 
 export const useGetReviewInfo = () => {
   const [page, setPage] = useState(1);
   const [lastPage, setLastPage] = useState(null);
   const [reviewList, setReviewList] = useState([]);
-  const userId = JSON.parse(window.localStorage.getItem("user")).id;
+  const userId = useRecoilValue(userIdAtom);
   const { request } = useApi("get", `reviews`, {
     userId,
     page: page - 1,
   });
 
-  const refineResponse = response => {
+  const refineResponse = (response: any) => {
     const info = response.data.items;
     const { totalPages } = response.data.meta;
     setReviewList(info);
