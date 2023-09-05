ecefe57971a99615555b87e0c65597e11a675d12
sujikim <sujikim@student.42seoul.kr>
2023-09-05 11:01:47 +0900

    fix: 검색결과 미리보기 전체 검색결과 더보기를 검색결과 없을때 보이지 않도록

--- src/component/utils/BookSearchPreview.tsx
+++ src/component/utils/BookSearchPreview.tsx
@@ -53,7 +53,7 @@ const BookSearchPreview = ({
         className="search-preview__more"
         to={`/search?search=${encodeURI(keyword)}`}
       >
-        {!isLoading && (
+        {!isLoading && totalCount && (
           <EmphasisInString
             wholeString={`전체 ${totalCount} 건 검색 결과 더보기`}
             emphasis={`${totalCount}`}
