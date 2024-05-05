9eb3d3e6ab1dfd00b6e82bc2163f89bd0bd650fe
jhj9109 <jhj91_09@naver.com>
2024-05-05 21:32:12 +0900

    fix: 사용 불가능 카메라 목록에서 삭제 등
    listVideoInputDevices()로 불러들인 디바이스 중 사용불가능한 것을 목록에서 제거
    카메라 변경시마다 reset()을 호출

--- src/component/utils/BarcodeReader.tsx
+++ src/component/utils/BarcodeReader.tsx
@@ -1,62 +1,93 @@
-import { useState, useRef, useEffect, ChangeEventHandler } from "react";
-import { BrowserMultiFormatReader, Result } from "@zxing/library";
 import "../../asset/css/BarcodeReader.css";
+import { useState, useRef, useEffect, ChangeEventHandler } from "react";
+import { BrowserMultiFormatReader, Result, BrowserQRCodeReader } from "@zxing/library";
 
 type Props = {
   wrapperClassName?: string;
   toDoAfterRead: (text: string) => void;
 };
 
+const updateNewData = <T,>(prevData: T, newData: T) =>
+  prevData === newData ? prevData : newData;
+
+const codeReader = new BrowserMultiFormatReader();
+
 const BarcodeReader = ({ toDoAfterRead, wrapperClassName = "" }: Props) => {
-  const [deviceList, setDeviceList] = useState<MediaDeviceInfo[]>([]);
-  const [selectedDevice, setSelectedDevice] = useState(deviceList[0]?.deviceId);
+  const [videoDeviceList, setVideoDeviceList] = useState<MediaDeviceInfo[]>([]);
+  const [selectedDeviceId, setSelectedDeviceId] = useState("");
   const videoRef = useRef<HTMLVideoElement>(null);
-  const codeReader = new BrowserMultiFormatReader();
 
   useEffect(() => {
-    codeReader
-      .listVideoInputDevices()
-      .then(videoInputDevices => {
-        setDeviceList(videoInputDevices);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    return () => {
-      codeReader.reset();
+    /** 바코드리더 초기 로드시, 사용가능한 카메라 목록을 가져옵니다. */
+    const loadVideoInputDeviceList = async () => {
+      let videoDeviceList: MediaDeviceInfo[] = [];
+      try {
+        videoDeviceList = await codeReader.listVideoInputDevices();
+      } catch (error) {
+        console.error(error);
+      }
+      setVideoDeviceList((prev) => updateNewData(prev, videoDeviceList));
     };
+    loadVideoInputDeviceList();
+    return () => codeReader.reset();
   }, []);
 
   useEffect(() => {
-    codeReader.decodeFromVideoDevice(
-      selectedDevice,
-      videoRef.current,
-      (result: Result) => {
-        result && toDoAfterRead(result.getText());
-      },
-    );
-  }, [selectedDevice]);
+    /** 바코드리더 초기 로드시 & 목록 변화시, 사용가능한 카메라 목록중 하나를 디폴트로 선택합니다. */
+    if (videoDeviceList.length) {
+      const firstVideoDeviceId = videoDeviceList[0].deviceId;
+      setSelectedDeviceId((prev) => updateNewData(prev, firstVideoDeviceId));
+    }
+  }, [videoDeviceList]);
+  
+  useEffect(() => {
+    /** 디바이스ID로 스트림을 받아옵니다. */
+    if (selectedDeviceId !== "" && videoRef.current) {
+      const setMedia = async (htmlVideoElement: HTMLVideoElement) => {
+        codeReader.reset(); /* 기존 연결 해제 */
+        const constraints = { video: { deviceId: { exact: selectedDeviceId } } };
+        let newVideoStream: MediaStream | null = null;
+        try {
+          /** 디바이스 목록엔 떴지만 사용불가시 throw */
+          newVideoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          htmlVideoElement.srcObject = newVideoStream;
+          htmlVideoElement.autoplay = true;
+        }
+        if (newVideoStream) {
+          /** 연결된 디바이스에 콜백함수를 전달해줍니다. */
+          codeReader.decodeFromVideoElementContinuously(
+            htmlVideoElement,
+            (result: Result) => result && toDoAfterRead(result.getText())
+          );
+        } else {
+          /** 사용 불가능한 디바이스는 선택지에서 제거합니다. */
+          alert(`해당 디바이스는 사용할 수 없습니다. (${selectedDeviceId})`)
+          setVideoDeviceList((prev) => prev.filter(prev => prev.deviceId !== selectedDeviceId));
+        }
+      };
+      setMedia(videoRef.current);
+    }
+  }, [selectedDeviceId]);
 
-  const onChangeSelect: ChangeEventHandler<HTMLSelectElement> = e => {
-    setSelectedDevice(e.currentTarget.value);
-  };
+  const handleSelectedIdChange: ChangeEventHandler<HTMLSelectElement> = (e) =>
+    setSelectedDeviceId((prev) => updateNewData(prev, e.target.value));
 
   return (
     <div className={`barcode-reader__wrapper ${wrapperClassName || ""}`}>
       <video id="video" ref={videoRef} muted />
       <select
         id="sourceSelect"
-        onChange={onChangeSelect}
-        value={selectedDevice}
+        onChange={handleSelectedIdChange}
+        value={selectedDeviceId}
       >
-        {deviceList.map(device => {
-          return (
-            <option key={device.deviceId} value={device.deviceId}>
-              {device.label}
-            </option>
-          );
-        })}
+        {(videoDeviceList ?? []).map((device) => (
+          <option key={device.deviceId} value={device.deviceId}>
+            {device.label}
+          </option>
+        ))}
       </select>
     </div>
   );
