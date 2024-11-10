b37336e766a1c2d1ce9c03336811bd1771794733
yeonseong <yeonseong.dev@gmail.com>
2024-11-10 14:41:16 +0900

    refactor: :zap: Search 컴포넌트 퍼포먼스 측정을 위해 Profiler 추가

--- src/component/search/Search.tsx
+++ src/component/search/Search.tsx
@@ -1,4 +1,4 @@
-import { useRef, useCallback } from "react";
+import { useRef, useCallback, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import Sort from "./Sort";
 import BookInfo from "./BookInfo";
@@ -13,6 +13,22 @@ import { useGetBooksInfoSearchUrl } from "../../api/books/useGetBooksInfoSearchU
 import { searchUrlQueryKeys } from "../../constant/key";
 import "../../asset/css/Books.css";
 import "../../asset/css/Search.css";
+import { Profiler } from 'react';
+
+const onRender = (
+  id: string,
+  phase: string,
+  actualDuration: number,
+  baseDuration: number,
+  startTime: number,
+  commitTime: number
+) => {
+  console.table([
+    { 항목: '컴포넌트', 값: id },
+    { 항목: '실제 렌더링 시간', 값: `${actualDuration}ms` },
+    { 항목: '기본 렌더링 시간', 값: `${baseDuration}ms` }
+  ]);
+};
 
 const Search = () => {
   const myRef = useRef<HTMLDivElement>(null);
@@ -53,55 +69,57 @@ const Search = () => {
   );
 
   return (
-    <main>
-      <SearchBanner setQuery={setQuery} />
-      <section className="search-section">
-        <div className="search-subtitle" ref={myRef}>
-          <SubTitle
-            subTitle={
-              query === null || query === ""
-                ? "전체 도서 목록"
-                : `'${query}' 도서 검색 결과`
-            }
-            alignItems="start"
-            description=""
-          />
-        </div>
-        <CategoryFilter
-          selectedCategory={categoryIndex}
-          categoryList={categoryList}
-        />
-        <div className="search-sort">
-          <Sort sort={sort || "title"} setSort={setSort} />
-        </div>
-        <section className="books">
-          {bookList.map(items => (
-            <BookInfo
-              key={items.id}
-              id={items.id}
-              isbn={items.isbn}
-              title={items.title}
-              author={items.author}
-              publisher={items.publisher}
-              image={items.image}
-              publishedAt={items.publishedAt}
-              category={items.category}
-              bread="검색"
+    <Profiler id="Search" onRender={onRender}>
+      <main>
+        <SearchBanner setQuery={setQuery} />
+        <section className="search-section">
+          <div className="search-subtitle" ref={myRef}>
+            <SubTitle
+              subTitle={
+                query === null || query === ""
+                  ? "전체 도서 목록"
+                  : `'${query}' 도서 검색 결과`
+              }
+              alignItems="start"
+              description=""
             />
-          ))}
+          </div>
+          <CategoryFilter
+            selectedCategory={categoryIndex}
+            categoryList={categoryList}
+          />
+          <div className="search-sort">
+            <Sort sort={sort || "title"} setSort={setSort} />
+          </div>
+          <section className="books">
+            {bookList.map(items => (
+              <BookInfo
+                key={items.id}
+                id={items.id}
+                isbn={items.isbn}
+                title={items.title}
+                author={items.author}
+                publisher={items.publisher}
+                image={items.image}
+                publishedAt={items.publishedAt}
+                category={items.category}
+                bread="검색"
+              />
+            ))}
+          </section>
+          <Pagination
+            lastPage={lastPage}
+            page={parseInt(page, 10) || 1}
+            setPage={setPage}
+            isReplaceUrl
+            scrollRef={myRef}
+          />
         </section>
-        <Pagination
-          lastPage={lastPage}
-          page={parseInt(page, 10) || 1}
-          setPage={setPage}
-          isReplaceUrl
-          scrollRef={myRef}
-        />
-      </section>
-      <section className="wish-book-wraper">
-        <WishBook />
-      </section>
-    </main>
+        <section className="wish-book-wraper">
+          <WishBook />
+        </section>
+      </main>
+    </Profiler>
   );
 };
 
