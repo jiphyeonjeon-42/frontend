ae3a4a367dc5035c5ecd75a9b105a73dafd03414
Misung Kim <misung178@gmail.com>
2024-11-13 17:57:01 +0900

    feature: 책 위치에 따른 선반위치 자동 scroll

--- src/component/book/location/BookShelf.tsx
+++ src/component/book/location/BookShelf.tsx
@@ -1,4 +1,5 @@
 import "~/asset/css/BookLocation.css";
+import { useRef, useEffect } from "react";
 
 type BookShelfProps = {
   index: number;
@@ -6,15 +7,24 @@ type BookShelfProps = {
 };
 
 const BookShelf = ({ index, highlight }: BookShelfProps) => {
+  const shelfRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (highlight && shelfRef.current) {
+      shelfRef.current.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+      });
+    }
+  }, [highlight]);
+
   return (
     <div
+      ref={highlight ? shelfRef : undefined}
       className={`${highlight ? "shelf_active" : ""} ${
         index <= 2 ? "book-shelf__big" : "book-shelf__small"
       } 
       }`}
-    >
-      {highlight}
-    </div>
+    ></div>
   );
 };
 
